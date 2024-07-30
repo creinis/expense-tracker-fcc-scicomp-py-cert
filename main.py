bc32fbf100c77e53516f11e84670e0ae6442c006
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:38:08 +0200

    feat: apply append method in expenses argument inside add_expenses function

--- main.py
+++ main.py
@@ -22,4 +22,19 @@ numbers = [1, 2, 3, 4]
 
 expenses = []
 
+# Step 3
+
+# The expenses parameter of your add_expense function will be a list of expenses. 
+# You want to be able to add items at the end of your list. For that you can use the .append() list method:
+
+my_list = [2, 4, 7]
+my_list.append(3)
+
+#In the example above, after appending 3, my_list would be [2, 4, 7, 3]. 
+# Replace pass with a call to the .append() method on the expenses list. 
+# Don't pass any arguments to .append() for now.
+
+def add_expense(expenses, amount, category):
+    expenses.append()
+
 
