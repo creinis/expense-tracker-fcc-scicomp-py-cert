c2d23cd0d0ec0e387a78c04174b8e15f5668aa3b
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:52:59 +0200

    feat: Inside the print_expenses function, create a for loop that iterates over each item in the expenses list. Use expense as the loop variable and move pass inside the loop body

--- main.py
+++ main.py
@@ -71,4 +71,13 @@ def add_expense(expenses, amount, category):
 def print_expenses(expenses):
     pass
 
+# Step 7
+
+# Inside the print_expenses function, create a for loop that iterates over each item in the expenses list. 
+# Use expense as the loop variable and move pass inside the loop body.
+
+def print_expenses(expenses):
+    for expense in expenses:
+        pass
+
 
