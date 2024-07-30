660c9af58401e97cb7338d260df2b61cbe024d95
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 18:28:55 +0200

    feat: Inside your while loop, call the input() function passing the string Enter your choice: as the argument, and assign the result to a variable named choice

--- main.py
+++ main.py
@@ -267,6 +267,23 @@ while True:
     print('4. Filter expenses by category')
     print('5. Exit')
 
+# Step 28
+
+# The input() function takes a user input and it returns the user input in the form of a string.
+
+# Inside your while loop, call the input() function passing the string Enter your choice: as the argument, 
+# and assign the result to a variable named choice.
+
+    def main():
+        expenses = []
+        while True:
+            print('\nExpense Tracker')
+            print('1. Add an expense')
+            print('2. List all expenses')
+            print('3. Show total expenses')
+            print('4. Filter expenses by category')
+            print('5. Exit')
+            choice = input('Enter your choice: ')
 
 
 
