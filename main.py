7b5e871086b24c2a3bd2a801d117052f97a96735
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 18:00:34 +0200

    test: use sum() together with map() and lambda functions to get the total amount of expenses. Modify print() call by passing sum(map(test, [2, 3, 5, 8])) as the argument

--- main.py
+++ main.py
@@ -155,4 +155,14 @@ map(lambda x: x * 2, [1, 2, 3])
 test = lambda x: x * 2
 print(map(test, [2, 3, 5, 8]))
 
+# Step 14
+
+# The sum() function returns the sum of the items in the iterable which is passed as its argument. 
+# You are going to use sum() together with map() and lambda functions to get the total amount of expenses.
+
+# For now, make a little test and modify your current print() call by passing sum(map(test, [2, 3, 5, 8])) as the argument.
+
+test = lambda x: x * 2
+print(sum(map(test, [2, 3, 5, 8])))
+
 
