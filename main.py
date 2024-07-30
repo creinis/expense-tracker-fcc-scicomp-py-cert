753180a43d28d82e50dff2aaca6d723b0f4863ab
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:56:34 +0200

    feat: Define function total_expenses that takes one parameter expenses

--- main.py
+++ main.py
@@ -108,4 +108,13 @@ def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
 
+# Step 10
+
+# You will need a function to calculate the total amount of expenses. 
+# Define a function named total_expenses that takes one parameter expenses. 
+# Fill the function body with a pass statement for now.
+
+def total_expenses(expenses):
+    pass
+
 
