9b5c9374a1e603de1a1cc156245f322d33541ed8
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:34:47 +0200

    feat: define add_expense function, passing expenses, amount and category

---
+++ main.py
@@ -0,0 +1,12 @@
+# Lambda
+
+# Step 1
+
+# In this project, you're going to build a simple, yet functional expense tracker in Python.
+
+# Start by defining a function named add_expense that takes three parameters: 
+# expenses, amount and category. Use the pass keyword to fill the function body.
+
+def add_expense(expenses, amount, category):
+    pass
+
