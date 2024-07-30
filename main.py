f0aa2a361c8014f338ea7e3901844c3e21ae971f
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:41:12 +0200

    feat: def function print_expenses that takes one parameter expenses. This function will later be used to display each expense in your list. Fill the body of your new function with a pass statement

--- main.py
+++ main.py
@@ -61,4 +61,14 @@ def add_expense(expenses, amount, category):
 def add_expense(expenses, amount, category):
     expenses.append({'amount': amount, 'category': category})
 
+# Step 6
+
+# Start by defining a function named print_expenses that takes one parameter expenses. 
+# This function will later be used to display each expense in your list.
+
+# Fill the body of your new function with a pass statement.
+
+def print_expenses(expenses):
+    pass
+
 
