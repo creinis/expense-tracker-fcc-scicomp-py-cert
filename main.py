50a9e95b0ad5532491cc091f9f8261f1fae83732
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:36:31 +0200

    feat: declare expenses as an empity list

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def add_expense(expenses, amount, category):
 
 numbers = [1, 2, 3, 4]
 
+# Create an empty list named expenses. You will use it to store each of your expenses.
+
+expenses = []
+
+
