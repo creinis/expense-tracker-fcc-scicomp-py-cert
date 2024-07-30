5e73523db2945e1e9f485ba0a8a5fbfffdfa7bfa
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:54:33 +0200

    feat: Modify the f-string expression to access the value of the amount key and the category key in the expense dictionary.

--- main.py
+++ main.py
@@ -92,4 +92,20 @@ def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expense}, Category: {expense}')
 
+# Step 9
+
+# You can access values in a dictionary through its keys. 
+# You need to use the bracket notation and include the key between the square brackets:
+
+my_dict = {"amount": 50.0, "category": "Food"}
+my_dict["amount"] # 50.0
+
+# You are currently interpolating the expense dictionary in your f-string. 
+# Modify the f-string expression to access the value of the amount key and the category key 
+# in the expense dictionary.
+
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
 
