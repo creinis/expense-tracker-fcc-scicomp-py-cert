334f65881a8643334588ae0cc5c1e73650c6281c
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:39:26 +0200

    feat: Create a dictionary with a key amount and value of the amount parameter and pass new dictionary to the .append() call.

--- main.py
+++ main.py
@@ -37,4 +37,20 @@ my_list.append(3)
 def add_expense(expenses, amount, category):
     expenses.append()
 
+# Step 4
+
+# A dictionary is another built-in data type in Python. 
+# A dictionary is a collection of data in the form of key-value pairs. 
+# Dictionaries are defined with curly braces {} and they contain key-value pairs separated by commas. 
+# Each key is followed by a colon : and the value:
+
+{"amount": 50.0, "category": "Food"}
+
+# In the example above, amount and category are keys, and 50.0 and Food are their the corresponding values.
+
+# Create a dictionary with a key amount and value of the amount parameter and pass your new dictionary to the .append() call.
+
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount})
+
 
