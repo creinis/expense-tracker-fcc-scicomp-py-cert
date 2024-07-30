76cd2ab82580a41ec987b48460dda5b429bfb5b5
creinis <115181527+creinis@users.noreply.github.com>
2024-07-30 17:53:49 +0200

    feat: Inside the for loop, replace pass with a print() call and pass it the following, f-string: f'Amount: {expense}, Category: {expense}'. # Leave the placeholders empty for now.

--- main.py
+++ main.py
@@ -80,4 +80,16 @@ def print_expenses(expenses):
     for expense in expenses:
         pass
 
+# Step 8
+
+# Next, you are going to display the details for each expense.
+
+# Inside the for loop, replace pass with a print() call and pass it the following 
+# f-string: f'Amount: {expense}, Category: {expense}'. 
+# Leave the placeholders empty for now.
+
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense}, Category: {expense}')
+
 
